081996afb47bea409d86e1f3f619a5aa58e34a1e
Joshua Sterner <josh@joshuasterner.com>
2020-07-01 00:01:02 -0700

    Started writing widget layout editor.

--- test.js
+++ test.js
@@ -6,7 +6,8 @@ function makeDiv(x, y, w, h, label) {
     div.style.height = h + "px";
     div.classList.add("widget");
     div.innerHTML = label;
-    document.body.appendChild(div);
+    document.getElementById("widgets").appendChild(div);
+    return div;
 }
 
 class Widget {
@@ -68,7 +69,7 @@ let a = new Widget("a", 64, 16);
 let b = new Widget("b", 32, 48);
 let c = new Widget("c", 128, 96);
 
-widgets = [panel, a, b, c];
+var widgets = [panel, a, b, c];
 
 // set widget relationships
 a.left.push(panel);
@@ -87,7 +88,7 @@ c.left.push(panel);
 c.right.push(panel);
 c.above.push(panel);
 c.above.push(b);
-c.above.push(panel);
+c.above.push(a);
 c.below.push(panel);
 c.parent = panel;
 
@@ -99,14 +100,179 @@ panel.x1 = panel.x0 + panel.width;
 panel.y0 = 0;
 panel.y1 = panel.y0 + panel.height;
 
-// set widget positions
-panel.children.forEach((child) => {
-    child.computeX();
-    child.computeY();
-});
-
-window.onload = () => {
+function findRootWidget() {
+    let root = null;
     widgets.forEach((widget) => {
+        if (!widget.parent) {
+            if (!root) {
+                root = widget;
+            } else {
+                alert("Multiple top-level widgets detected");
+            }
+        }
+    });
+    return root;
+}
+
+function drawWidgets() {
+
+    let widgets_element = document.getElementById("widgets");
+
+    widgets_element.innerHTML = "";
+
+    let root = findRootWidget();
+    if (!root) {
+        return;
+    }
+
+    // set widget positions
+    root.children.forEach((child) => {
+        child.computeX();
+        child.computeY();
+    });
+
+    let widgets_width = 0;
+    let widgets_height = 0;
+    widgets.forEach((widget) => {
+        widgets_width = Math.max(widgets_width, widget.x0 + widget.width);
+        widgets_height = Math.max(widgets_height, widget.y0 + widget.height);
         makeDiv(widget.x0, widget.y0, widget.width, widget.height, widget.label);
     });
+    widgets_element.style.width = widgets_width + "px";
+    widgets_element.style.height = widgets_height + "px";
+
+}
+
+function clearWidgetList(widget_list) {
+    let to_remove = [];
+    for (child of widget_list.children) {
+        if (child.classList.contains("widget-list-item") || child.classList.contains("widget-list-space")) {
+            to_remove.push(child);
+        }
+    }
+    to_remove.forEach((child) => {
+        widget_list.removeChild(child);
+    });
+}
+
+function removeWidget(widget) {
+    widgets = widgets.filter(i => i != widget);
+    widgets.forEach((i) => {
+        if (i.parent == widget) {
+            i.parent = null;
+        }
+        i.children = i.children.filter(j => j != widget);
+
+        if (i.left.length == 1 && i.left[0] == widget) {
+            i.left[0] = i.parent;
+        } else {
+            i.left = i.left.filter(j => j != widget);
+        }
+
+        if (i.right.length == 1 && i.right[0] == widget) {
+            i.right[0] = i.parent;
+        } else {
+            i.right = i.right.filter(j => j != widget);
+        }
+        
+        if (i.above.length == 1 && i.above[0] == widget) {
+            i.above[0] = i.parent;
+        } else {
+            i.above = i.above.filter(j => j != widget);
+        }
+
+        if (i.below.length == 1 && i.below[0] == widget) {
+            i.below[0] = i.parent;
+        } else {
+            i.below = i.below.filter(j => j != widget);
+        }
+    });
+    processWidgets();
+}
+
+function makeWidgetListItem(widget, edit_select) {
+    let item = document.createElement("div");
+    item.draggable = true;
+    if (edit_select) {
+        item.onclick = () => {
+            console.log("widget button clicked");
+            editWidget(widget);
+        };
+    }
+    item.classList.add("widget-list-item");
+    item_text = document.createElement("div");
+    item_text.classList.add("widget-list-item-text");
+    item_text.innerHTML = widget.label;
+    item_remove_button = document.createElement("div");
+    item_remove_button.classList.add("widget-list-item-remove");
+    item_remove_button.innerHTML = "X";
+    item.appendChild(item_text);
+    item.appendChild(item_remove_button);
+    item_remove_button.onclick = function(e) {
+        e.stopPropagation();
+        console.log("remove button clicked");
+        removeWidget(widget);
+    };
+    return item;
+}
+
+function appendWidgetList(dom_element, widget_list, edit_select) {
+    if (widget_list.length == 0) {
+        return;
+    }
+    dom_element.appendChild(makeWidgetListItem(widget_list[0], edit_select));
+    for (let i = 1; i < widget_list.length; i++) {
+        let space = document.createElement("span");
+        space.classList.add("widget-list-space");
+        dom_element.appendChild(space);
+        dom_element.appendChild(makeWidgetListItem(widget_list[i], edit_select));
+    }
+}
+
+function updateWidgetList(dom_element, widget_list, edit_select) {
+    clearWidgetList(dom_element);
+    appendWidgetList(dom_element, widget_list, edit_select);
+}
+
+function processWidgets() {
+    drawWidgets();
+    let widget_list = document.getElementById("main-widget-list");
+    updateWidgetList(widget_list, widgets, true);
+    let widget_datalist = document.getElementById("widget-datalist");
+    widget_datalist.innerHTML = "";
+    widgets.forEach((widget) => {
+        let i = document.createElement("option");
+        i.value = widget.label;
+        widget_datalist.appendChild(i);
+    });
+    if (editing_widget) {
+        editWidget(editing_widget);
+    }
+}
+
+var editing_widget = null;
+
+function editWidget(widget) {
+    editing_widget = widget;
+
+    widget_name_field = document.getElementById("widget-name-field");
+    widget_name_field.value = widget.label;
+    widget_parent_field = document.getElementById("widget-parent");
+    if (!widget.parent) {
+        widget_parent_field.value = "";
+    } else {
+        widget_parent_field.value = widget.parent.label;
+    }
+    updateWidgetList(document.getElementById("widget-editor-children"), widget.children);
+    updateWidgetList(document.getElementById("widget-editor-left"), widget.left);
+    updateWidgetList(document.getElementById("widget-editor-right"), widget.right);
+    updateWidgetList(document.getElementById("widget-editor-above"), widget.above);
+    updateWidgetList(document.getElementById("widget-editor-below"), widget.below);
+}
+
+window.onload = () => {
+    processWidgets();
+    if (widgets.length > 0) {
+        editWidget(widgets[0]);
+    }
 }
